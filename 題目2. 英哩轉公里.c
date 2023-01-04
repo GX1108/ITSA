b126badfc502d579efcac62300c084c9e9754828
GX1108 <121872069+GX1108@users.noreply.github.com>
2023-01-04 08:05:20 +0800

    Add files via upload

---
+++ 題目2. 英哩轉公里.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int km;
+    while(scanf("%d",&km)!=EOF)
+    {
+        double mile;
+        mile=km*1.6;
+        printf("%.1f\n",mile);
+    }
+}
