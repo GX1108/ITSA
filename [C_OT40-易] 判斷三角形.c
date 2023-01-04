cb22d877ef8706895aee2b4ed753675448b91ba6
GX1108 <121872069+GX1108@users.noreply.github.com>
2023-01-04 09:52:54 +0800

    Add files via upload

---
+++ [C_OT40-易] 判斷三角形.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    double ans;
+    while(scanf("%d\n%d\n%d",&a,&b,&c)!=EOF)
+    {
+        if((a+b)>c&&(b+c)>a&&(a+c)>b)
+            printf("Bigger than the third side\n");
+        else
+            printf("Less than the third side\n");
+
+    }
+}
