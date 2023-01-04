605f2b6aa7364759370b96ef6c8df5d51aa63494
GX1108 <121872069+GX1108@users.noreply.github.com>
2023-01-04 09:07:39 +0800

    Add files via upload

---
+++ [C_OT13-易] 斜率.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x1,y1,x2,y2;
+    while(scanf("%d %d\n%d %d",&x1,&y1,&x2,&y2)!=EOF)
+    {
+    int m=(y1-y2)/(x1-x2);
+        printf("%d\n",m);
+    }
+
+}
